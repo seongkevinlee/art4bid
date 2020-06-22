95806bf1b3dfa08fb63de3b7053adcae407998ef
heegupark <slhead@gmail.com>
2020-06-22 11:23:52 -0700

    Completed collecting stater files

---
+++ client/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/health-check')
+      .then(res => res.json())
+      .then(data => this.setState({ message: data.message || data.error }))
+      .catch(err => this.setState({ message: err.message }))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+}
