b1ca26e2bb27ceb542ece3339e6c123f80d05b18
Sarah Choung <sarahjchoung@gmail.com>
2020-06-25 01:01:09 +0000

    halfway through hardcoding the layout

--- client/components/specific-post.jsx
+++ client/components/specific-post.jsx
@@ -11,26 +11,43 @@ export default class SpecificPost extends React.Component {
   render() {
     return (
       <div>
-        <header className="post-header text-center d-flex justify-content-around align-items-center">
-          <div className="header-icon-container">
+        <header className="post-header text-center d-flex justify-content-between align-items-center pt-3 pl-3 pr-3 pb-2">
+          <div className="header-icon-container d-flex flex-column justify-content-center mt-2">
             <img
               onClick={() => this.props.setView('search')}
               className="header-icon" src="./images/kindpng.png"></img>
-            <p></p>
+            <p className="following m-0 invisible">go back</p>
           </div>
-          <div>
+          <div className="post-header-text">
             <p className="m-0">&ldquo;Vendetta&rdquo;</p>
             <p className="m-0">by</p>
             <p className="m-0">User</p>
           </div>
-          <div className="header-icon-container text-center">
+          <div className="header-icon-container d-flex flex-column justify-content-center mt-2">
             <img className="header-icon" src="./images/fire-alt-solid.svg"></img>
-            <p>watching</p>
+            <p className="following pt-1 m-0"><span>420</span> Watching</p>
           </div>
         </header>
-        <section className="post-section">
-          {/* <img className="post-image" src="https://i.picsum.photos/id/894/600/600.jpg?hmac=wJg2bcsTpfgAb0eYMLFbhzwiEUMTMBwtBf-yvdY491k"></img> */}
-        </section>
+        <div className="post-body">
+          <div className="art-info">
+            <div className="post-image-container">
+              <img src="https://i.picsum.photos/id/72/800/800.jpg?hmac=yUlan-c5Ns8rxfyTLLnVDNHAXBo68XLUgnr_IrGCgS0"></img>
+            </div>
+            <div className="post-description">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro aspernatur odio hic ex! Nulla mollitia odit assumenda nemo voluptates, tempora ullam, sint quaerat nesciunt dolorum pariatur consequuntur maxime corrupti quod?</p>
+            </div>
+          </div>
+        </div>
+        <div className="post-bid-info d-flex">
+          <div className="d-flex flex-column">
+            <button>$80.00</button>
+            <button>Submit Bid</button>
+            <button>Message</button>
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     );
   }
