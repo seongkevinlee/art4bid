ffb38f2a8beb11f8e5193aafa9e856b2f76c1cb7
heegupark <slhead@gmail.com>
2020-07-03 14:55:52 -0700

    Completed bugfix for search post

--- client/components/search-page.jsx
+++ client/components/search-page.jsx
@@ -8,6 +8,10 @@ export default class SearchPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      formData: {
+        search: ''
+      },
+      message: '',
       search: '',
       paintings: [],
       photographs: [],
@@ -17,7 +21,6 @@ export default class SearchPage extends React.Component {
     };
     this.getThumbnails = this.getThumbnails.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.handleSearchClick = this.handleSearchClick.bind(this);
     this.getZipcodesByCity = this.getZipcodesByCity.bind(this);
     this.getZipcodesByZipcodeWithinRadius = this.getZipcodesByZipcodeWithinRadius.bind(this);
     this.searchPostsByZipcodes = this.searchPostsByZipcodes.bind(this);
@@ -130,39 +133,21 @@ export default class SearchPage extends React.Component {
       event.target.value = event.target.value.trim();
     }
     this.setState({
-      search: event.target.value
-    });
-  }
-
-  handleSearchClick() {
-    const { search } = this.state;
-    if (!isNaN(Number(search)) && search && search.trim().length > 0) {
-      // radius mile is set to 5 as default
-      this.getZipcodesByZipcodeWithinRadius(search, 5);
-    } else if (search) {
-      // const city = search.split(',')[0].trim().toUpperCase();
-      let city = search.split(',')[0].trim();
-      if (city.includes('.')) {
-        city = city.split('.')[1].trim();
+      search: event.target.value,
+      formData: {
+        search: event.target.value
       }
-      const state = search.split(',')[1] ? search.split(',')[1].trim().substring(0, 2) : '';
-      this.getZipcodesByCity(city, state);
-    } else {
-      this.showMessage('please click brush!', 1000);
-    }
-    this.setState({
-      search: ''
     });
   }
 
   showMessage(message, time) {
     setTimeout(() => {
       this.setState({
-        search: ''
+        message: ''
       });
     }, time);
     this.setState({
-      search: message
+      message: message
     });
   }
 
@@ -177,153 +162,157 @@ export default class SearchPage extends React.Component {
     this.getThumbnails('other');
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    // this.handleSearchClick();
+  handleSubmit(event) {
+    event.preventDefault();
+    const { search } = this.state.formData;
+    if (!isNaN(Number(search)) && search && search.trim().length > 0) {
+      // radius mile is set to 5 as default
+      this.getZipcodesByZipcodeWithinRadius(search, 5);
+    } else if (search) {
+      // const city = search.split(',')[0].trim().toUpperCase();
+      let city = search.split(',')[0].trim();
+      if (city.includes('.')) {
+        city = city.split('.')[1].trim();
+      }
+      // Temporarily if state is empty, the default is 'CA'
+      const state = search.split(',')[1] ? search.split(',')[1].trim().substring(0, 2) : 'CA';
+      this.getZipcodesByCity(city, state);
+    } else if (search === undefined) {
+      this.showMessage('please click brush!', 1000);
+    } else if (search.length === 0) {
+      this.showMessage('please enter keyword!', 1000);
+    }
+    this.setState({
+      formData: {
+        search: ''
+      }
+    });
   }
 
   render() {
-    const { handleSearchChange, handleSearchClick, loadPostsAgain } = this;
-    const { search, paintings, photographs, other } = this.state;
-    if (paintings.length > 0 || photographs.length > 0 || other.length > 0) {
-      return (
-        <div className="non-nav">
-          <div>
-            <div className="text-center pt-3">
-              <div className="position-relative">
-                <Autocomplete
-                  autoFocus
-                  types={['(regions)']}
-                  onPlaceSelected={place => {
-                    this.setState({
-                      search: place.formatted_address
-                    });
-                  }}
-                  componentRestrictions={{ country: 'us' }}
-                  className="mx-auto search-bar text-center w-75 border-0 pt-2 pb-2 form-control"
-                  type="text"
-                  name="search"
-                  placeholder="search"
-                  value={search || ''}
-                  onChange={handleSearchChange}/>
-                <img
-                  alt="search-button"
-                  type="button"
-                  className="search-mag position-absolute cursor-pointer"
-                  src="./images/search-brush.png"
-                  onClick={handleSearchClick}></img>
-              </div>
-            </div>
+    const { handleSearchChange, loadPostsAgain, handleSubmit } = this;
+    const { formData, message, paintings, photographs, other } = this.state;
+    return (
+      <div className="non-nav">
+        <div>
+          {message
+            ? (
+              <form className="text-center pt-3">
+                <div className="position-relative">
+                  <input
+                    disabled
+                    className="mx-auto search-bar text-center w-75 border-0 pt-2 pb-2 form-control"
+                    placeholder={message}/>
+                  <button
+                    disabled
+                    className="search-mag position-absolute cursor-pointer search-button">
+                    <img
+                      alt="search-button"
+                      className="search-mag cursor-pointer"
+                      src="./images/search-brush.png"></img>
+                  </button>
+                </div>
+              </form>
+            )
+            : (
+              <form className = "text-center pt-3" onSubmit = {e => handleSubmit(e) }>
+                <div className = "position-relative">
+                  <Autocomplete
+                    autoFocus
+                    types = { ['(regions)'] }
+                    onPlaceSelected = {place => {
+                      this.setState({
+                        search: place.formatted_address
+                      });
+                    }}
+                    formdata={formData}
+                    componentRestrictions={{ country: 'us' }}
+                    className="mx-auto search-bar text-center w-75 border-0 pt-2 pb-2 form-control"
+                    type="text"
+                    name="search"
+                    placeholder="search"
+                    value={formData.search}
+                    onChange={e => handleSearchChange(e)} />
+                  <button
+                    type="submit"
+                    className="search-mag position-absolute cursor-pointer search-button">
+                    <img
+                      alt="search-button"
+                      className="search-mag cursor-pointer"
+                      src="./images/search-brush.png"></img>
+                  </button>
+                </div>
+              </form>
+            )
+          }
+        </div>
+        <div className="d-flex mt-4 justify-content-around text-center" style={{ marginBottom: '6px' }}>
+          <div className="column-label align-text-bottom">
+            <h6>Paintings</h6>
           </div>
-          <div className="d-flex mt-4 justify-content-around text-center" style={{
-            marginBottom: '6px'
-          }}>
-            <div className="column-label align-text-bottom">
-              <h6>Paintings</h6>
-            </div>
-            <div className="column-label">
-              <h6>Photographs</h6>
-            </div>
-            <div className="column-label">
-              <h6>Other</h6>
-            </div>
+          <div className="column-label">
+            <h6>Photographs</h6>
           </div>
-          <div className="thumbnail-column-container d-flex justify-content-around">
-            {
-              paintings.length > 0
-                ? (
-                  <PaintingsThumbnailColumn
-                    // thumbnails={paintings}
-                    setView={this.props.setView}
-                    getPostInfo={this.props.getPostInfo}
-                  />
-                )
-                : <div className="flex-column thumbnail-column">
-                  <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
-                </div>
-            }
-            {
-              photographs.length > 0
-                ? (
-                  <PhotographsThumbnailColumn
-                    // thumbnails={photographs}
-                    setView={this.props.setView}
-                    getPostInfo={this.props.getPostInfo}
-                  />
-                )
-                : <div className="flex-column thumbnail-column">
-                  <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
-                </div>
-            }
-            {
-              other.length > 0
-                ? (
-                  <OtherThumbnailColumn
-                    // thumbnails={other}
-                    setView={this.props.setView}
-                    getPostInfo={this.props.getPostInfo}
-                  />
-                )
-                : <div className="flex-column thumbnail-column">
-                  <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
-                </div>
-            }
+          <div className="column-label">
+            <h6>Other</h6>
           </div>
         </div>
-      );
-    } else {
-      return (
-        <>
-          <div>
-            <div className="text-center pt-3">
-              <div className="position-relative">
-                <Autocomplete
-                  autoFocus
-                  types={['(regions)']}
-                  onPlaceSelected={place => {
-                    this.setState({
-                      search: place.formatted_address
-                    });
-                  }}
-                  componentRestrictions={{ country: 'us' }}
-                  className="mx-auto search-bar text-center w-75 border-0 pt-2 pb-2 form-control"
-                  type="text"
-                  name="search"
-                  placeholder="search"
-                  value={search || ''}
-                  onChange={handleSearchChange} />
-                <img
-                  alt="search-button"
-                  type="button"
-                  className="search-mag position-absolute cursor-pointer"
-                  src="./images/search-brush.png"
-                  onClick={handleSearchClick}></img>
+        {(paintings.length > 0 || photographs.length > 0 || other.length > 0)
+          ? (
+            <div className="thumbnail-column-container d-flex justify-content-around">
+              {
+                paintings.length > 0
+                  ? (
+                    <PaintingsThumbnailColumn
+                      // thumbnails={paintings}
+                      setView={this.props.setView}
+                      getPostInfo={this.props.getPostInfo}
+                    />
+                  )
+                  : <div className="flex-column thumbnail-column">
+                    <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
+                  </div>
+              }
+              {
+                photographs.length > 0
+                  ? (
+                    <PhotographsThumbnailColumn
+                      // thumbnails={photographs}
+                      setView={this.props.setView}
+                      getPostInfo={this.props.getPostInfo}
+                    />
+                  )
+                  : <div className="flex-column thumbnail-column">
+                    <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
+                  </div>
+              }
+              {
+                other.length > 0
+                  ? (
+                    <OtherThumbnailColumn
+                      // thumbnails={other}
+                      setView={this.props.setView}
+                      getPostInfo={this.props.getPostInfo}
+                    />
+                  )
+                  : <div className="flex-column thumbnail-column">
+                    <p className="h6 mt-4 px-1 text-secondary">There are no images</p>
+                  </div>
+              }
+            </div>
+          )
+          : (
+            <div className="text-center">
+              <p className="mt-5 text-center mt-3">No posts are found</p>
+              <div>
+                <button
+                  className="btn btn-sm btn-outline-danger"
+                  onClick={loadPostsAgain}>Try Again</button>
               </div>
             </div>
-          </div>
-          <div className="d-flex mt-4 justify-content-around text-center" style={{
-            marginBottom: '6px'
-          }}>
-            <div className="column-label align-text-bottom">
-              <h6>Paintings</h6>
-            </div>
-            <div className="column-label">
-              <h6>Photographs</h6>
-            </div>
-            <div className="column-label">
-              <h6>Other</h6>
-            </div>
-          </div>
-          <div className="text-center">
-            <p className="mt-5 text-center mt-3">No posts are found</p>
-            <div>
-              <button
-                className="btn btn-sm btn-outline-danger"
-                onClick={loadPostsAgain}>Try Again</button>
-            </div>
-          </div>
-        </>
-      );
-    }
+          )
+        }
+      </div>
+    );
   }
 }
